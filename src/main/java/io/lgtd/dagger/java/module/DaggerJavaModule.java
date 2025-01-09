baa07f122fc0b7432a7b902ee9d7145d9a693eb2
Yves Brissaud <gh@lgtd.io>
2025-01-09 10:05:21 +0100

    add annotation to describe the module
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- src/main/java/io/lgtd/dagger/java/module/DaggerJavaModule.java
+++ src/main/java/io/lgtd/dagger/java/module/DaggerJavaModule.java
@@ -5,16 +5,21 @@ import io.dagger.client.Container;
 import io.dagger.client.DaggerQueryException;
 import io.dagger.client.Directory;
 import io.dagger.module.Base;
+import io.dagger.module.annotation.Function;
+import io.dagger.module.annotation.Object;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+/// Dagger Java Module main object
+@Object
 public class DaggerJavaModule extends Base {
     public DaggerJavaModule(Client dag) {
         super(dag);
     }
 
     /// Returns a container that echoes whatever string argument is provided
+    @Function
     public Container containerEcho(String stringArg) {
         return dag.container()
                 .from("alpine:latest")
@@ -22,6 +27,7 @@ public class DaggerJavaModule extends Base {
     }
 
     /// Returns lines that match a pattern in the files of the provided Directory
+    @Function
     public String grepDir(Directory directoryArg, String pattern) throws InterruptedException, ExecutionException, DaggerQueryException {
         return dag.container()
                 .from("alpine:latest")
---
+++ src/main/java/io/lgtd/dagger/java/module/package-info.java
@@ -0,0 +1,5 @@
+/// Dagger Java Module example
+@Module(value = "DaggerJavaModule")
+package io.lgtd.dagger.java.module;
+
+import io.dagger.module.annotation.Module;
