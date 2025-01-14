8f6145c356dc12b1902513ba2b2fb48736685747
Yves Brissaud <gh@lgtd.io>
2025-01-14 17:08:48 +0100

    fmt
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- src/main/java/io/lgtd/dagger/java/module/DaggerJavaModule.java
+++ src/main/java/io/lgtd/dagger/java/module/DaggerJavaModule.java
@@ -14,26 +14,26 @@ import java.util.concurrent.ExecutionException;
 /// Dagger Java Module main object
 @Object
 public class DaggerJavaModule extends Base {
-    public DaggerJavaModule(Client dag) {
-        super(dag);
-    }
+  public DaggerJavaModule(Client dag) {
+    super(dag);
+  }
 
-    /// Returns a container that echoes whatever string argument is provided
-    @Function
-    public Container containerEcho(String stringArg) {
-        return dag.container()
-                .from("alpine:latest")
-                .withExec(List.of("echo", stringArg));
-    }
+  /// Returns a container that echoes whatever string argument is provided
+  @Function
+  public Container containerEcho(String stringArg) {
+    return dag.container()
+        .from("alpine:latest")
+        .withExec(List.of("echo", stringArg));
+  }
 
-    /// Returns lines that match a pattern in the files of the provided Directory
-    @Function
-    public String grepDir(Directory directoryArg, String pattern) throws InterruptedException, ExecutionException, DaggerQueryException {
-        return dag.container()
-                .from("alpine:latest")
-                .withMountedDirectory("/mnt", directoryArg)
-                .withWorkdir("/mnt")
-                .withExec(List.of("grep", "-R", pattern, "."))
-                .stdout();
-    }
+  /// Returns lines that match a pattern in the files of the provided Directory
+  @Function
+  public String grepDir(Directory directoryArg, String pattern) throws InterruptedException, ExecutionException, DaggerQueryException {
+    return dag.container()
+        .from("alpine:latest")
+        .withMountedDirectory("/mnt", directoryArg)
+        .withWorkdir("/mnt")
+        .withExec(List.of("grep", "-R", pattern, "."))
+        .stdout();
+  }
 }
